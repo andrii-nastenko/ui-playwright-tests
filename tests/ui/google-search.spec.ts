e2146eb7be10be31182c9a172e7c30bd18bf765a
Andrii Nastenko <andrew1991n@gmail.com>
2023-04-15 01:32:27 +0300

    replaced string helper

--- tests/ui/google-search.spec.ts
+++ tests/ui/google-search.spec.ts
@@ -1,7 +1,7 @@
 import {expect} from '@playwright/test';
 import {HomePageActions} from 'src/ui/pages/home-page/home-page-actions';
 import {ResultsPageSelectors} from 'src/ui/pages/results-page/results-page-selectors';
-import {generateLoremWord} from 'src/helpers/generator';
+import {generateString} from 'src/helpers/generator';
 import {test} from 'fixtures/ui-hooks';
 
 test.describe('Perform google search:', () => {
@@ -21,7 +21,7 @@ test.describe('Perform google search:', () => {
   });
 
   test('Check google search result', async () => {
-    const word = generateLoremWord(5);
+    const word = generateString(5);
     await homePageActions.enterSearchText(word);
     await homePageActions.startSearch();
 
