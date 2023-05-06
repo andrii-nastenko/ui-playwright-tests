b399ea7f55deb733ca1c1b4e33266458fd92177b
Andrii Nastenko <andrew1991n@gmail.com>
2023-05-06 22:23:38 +0300

    fixtures updates

--- src/helpers/fixtures.ts
+++ src/helpers/fixtures.ts
@@ -1,9 +1,17 @@
 import {test as base} from '@playwright/test';
 import {BaseClass} from 'src/ui/base-class';
 
-// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-const test = base.extend<{globalHooks: void}>({
-  globalHooks: [
+interface Fixtures {
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  traces: void;
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  skipMediaDownloads: void;
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  trelloLogin: void;
+}
+
+const test = base.extend<Fixtures>({
+  traces: [
     async ({browser}, use, testInfo) => {
       /** 'beforeEach' global hook starts here */
       for (const context of browser.contexts()) {
@@ -35,6 +43,19 @@ const test = base.extend<{globalHooks: void}>({
       for (const context of browser.contexts()) {
         await context.tracing.stop();
       }
+    },
+    {scope: 'test', auto: true},
+  ],
+  skipMediaDownloads: [
+    async ({browser}, use) => {
+      for (const context of browser.contexts()) {
+        for (const page of context.pages()) {
+          /** make pages load faster by skipping images and fonts downloading */
+          const baseClass = new BaseClass(page);
+          await baseClass.stopRequest('**/*.{png,jpg,jpeg,webp,svg,gif}');
+        }
+      }
+      await use();
     },
     {scope: 'test', auto: true},
   ],
